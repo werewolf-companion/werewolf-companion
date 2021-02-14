80a1e5f1fb56355d1da97862b1466ad095697b63
ApteryxXYZ <apteryx.xyz@gmail.com>
2021-02-15 01:58:58 +1300

    [.gitignore,daily,work]: unignore daily and work commands

---
+++ src/commands/currency/daily.js
@@ -0,0 +1,48 @@
+const random = require('weighted-random'),
+    ms = require('pretty-ms');
+
+module.exports = class Daily extends client.commandManager.Command {
+    constructor() {
+        super({
+            name: module.filename.split('/').pop().slice(0, -3),
+            category: module.filename.split('/').slice(-2)[0],
+            cooldown: ms('23.75h'),
+            aliases: []
+        })
+    }
+
+    async run({ message, user }) {
+        let now = Date.now(),
+            lastDaily = user.cooldowns.daily,
+            twoDay = ms('2d');
+
+        let streak = user.stats.streak.daily;
+        if (now - lastDaily > twoDay) {
+            database.users.set(user.id, 1, 'stats.streak.daily');
+            streak = 1;
+        } else database.users.math(user.id, '+', 1, 'stats.streak.daily')
+            && streak++;
+
+        streak = streak + (user.balance.token * 1.5);
+        let reward = this.generateReward(streak > 100 ? 100 : streak),
+            description = new String('');
+
+        if (['gold', 'rose', 'gem'].includes(reward.item)) {
+            database.users.math(user.id, '+', reward.amount, `balance.${reward.item}`);
+            terminal.currency(`${user.tag} (${user.id}) claimed their daily reward of ${reward.amount} ${reward.item}, they now have ${user.balance[reward.item] + reward.amount}.`);
+            description = `You claimed your daily reward and got ${reward.amount} ${message.emote(reward.item)}. You're on a ${streak} day streak.`;
+        }
+
+        return message.send({ message, title: 'Daily Reward', description, footer: 'Come back tomorrow to claim your next reward.' });
+    }
+
+    generateReward(streak) {
+        streak = Math.round(streak - 1);
+        let items = [
+            { item: 'gold', amount: Math.ceil(25 + ((streak / 50) * 100)), weight: 100 },
+            { item: 'rose', amount: Math.floor(1 + (streak / 8)), weight: 25 + ((streak - 1) / 2.75) },
+            { item: 'gem', amount: Math.floor(1 + (streak / 40)), weight: streak > 30 ? 5 + (streak / 3.25) : 0 }
+        ];
+        return items[random(items.map(x => x.weight))];
+    }
+}
---
+++ src/commands/currency/work.js
@@ -0,0 +1,46 @@
+const LIST = require('../../json/jobs.json'),
+    txtgen = require('random-words'),
+    ms = require('pretty-ms');
+
+module.exports = class Work extends client.commandManager.Command {
+    constructor() {
+        super({
+            name: module.filename.split('/').pop().slice(0, -3),
+            category: module.filename.split('/').slice(-2)[0],
+            cooldown: ms('1 hour'),
+            aliases: []
+        })
+    }
+
+    async run({ message, args, user }) {
+        let job = LIST.find(j => j.title === user.job.title);
+        if (!job) return Object.merge(message.send(`You don't have a job, get one using the \`${message.prefix}job apply <job>\` command. You can view a list of jobs using \`${message.prefix}jobs\`.`), { skipCooldown: true });
+        return this.startJob(message, user, job);
+    }
+
+    startJob(message, user, job) {
+        let sentence = txtgen({ exactly: 5, join: ' ', maxLength: 5 }).replace(/[?!;:,.'"-]/g, '');
+        let time = 5000 + sentence.length * 500,
+        filter = m => m.author.id === message.author.id,
+        { max, min } = job;
+
+        message.send({ message, title: 'Get Working', description: 'Retype the following 5 words:', image: encodeURI(`https://img.shields.io/badge/-${sentence}-white.png`) });
+        return message.channel.awaitMessages(filter, { max: 1, time, errors: ['time'] }).then(response => {
+            let msg = response.first(),
+                success = msg.content === sentence;
+            if (!success) max = max / 3, min = min / 3;
+            return this.finishJob(message, user, max, min, job.currency, success);
+        }).catch(response => this.finishJob(message, user, max / 3, min / 3, job.currency, false));
+    }
+
+    finishJob(message, user, max, min, currency, success) {
+        let payment = Math.floor(Math.random() * (max - min) + min);
+        payment = payment > 0 ? payment : 1;
+        database.users.set(user.id, Object.merge(user,
+            { job: { hours: ++user.job.hours }, balance: { [currency]: user.balance[currency] + payment } }));
+        terminal.currency(`${user.tag} (${user.id}) worked and earned ${payment} ${currency}.`);
+
+        if (success) return message.send(`You did a good job and earned ${payment} ${message.emote(currency)}, keep it up!`);
+        else return message.send(`You did a bad job but what's sad is that your boss still decided to pay you ${payment} ${message.emote(currency)}.`);
+    }
+}
