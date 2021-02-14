2c74a330050584db7ab13b7aa7392f179e32c4f0
ApteryxXYZ <apteryx.xyz@gmail.com>
2021-02-15 01:54:22 +1300

    [rose]: added ability to exchange sendable roses

--- src/commands/inventory/rose.js
+++ src/commands/inventory/rose.js
@@ -20,17 +20,33 @@ module.exports = class Rose extends client.commandManager.Command {
         if (!target) return message.send('No user was found for what was inputted.');
         if (target.id === user.id) return message.send('You cannot send yourself a rose!');
 
-        database.users.set(user.id, Object.merge(user, {
-            stats: { rose: { sent: user.stats.rose.sent + quantity }},
-            inventory: { 'i1': inventory['i1'] - quantity }
-        }))
-        database.users.set(target.id, Object.merge(target, {
-            stats: { rose: { received: target.stats.rose.received + quantity }},
-            balance: { rose: target.balance.rose + quantity }
-        }))
+        if (target.id === client.user.id) {
+            if (!quantity || quantity % 2 !== 0) return message.send('When trading in roses, quantity must be an even number.');
+
+            database.users.set(user.id, Object.merge(user, {
+                stats: { rose: { sent: user.stats.rose.sent + quantity } },
+                inventory: { 'i1': inventory['i1'] - quantity },
+                balance: { rose: user.balance.rose + (quantity / 2) }
+            }))
+
+            database.users.set(target.id, Object.merge(target, {
+                stats: { rose: { received: target.stats.rose.received + quantity } },
+                balance: { rose: target.balance.rose + quantity }
+            }))
+        } else {
+            database.users.set(user.id, Object.merge(user, {
+                stats: { rose: { sent: user.stats.rose.sent + quantity } },
+                inventory: { 'i1': inventory['i1'] - quantity }
+            }))
+
+            database.users.set(target.id, Object.merge(target, {
+                stats: { rose: { received: target.stats.rose.received + quantity } },
+                balance: { rose: target.balance.rose + quantity }
+            }))
+        }
 
         terminal.inventory(`${user.tag} (${user.id}) sent ${target.tag} (${target.id}) ${quantity} rose(s).`);
-        return message.send(`You successfully sent ${quantity} ${message.emote('rose')} to ${target.tag}.`);
+        return message.send(`You successfully sent ${quantity} ${message.emote('rose')} to ${target.tag}. ` + (target.id === client.user.id ? `In exchange you received ${quantity / 2} spendable ${message.emote('rose')}.` : ''));
     }
 
 
