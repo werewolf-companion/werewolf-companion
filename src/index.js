6ae7f95cf54329a793d532390ca8872b23a942cb
ApteryxXYZ <apteryx.xyz@gmail.com>
2021-03-06 19:12:58 +1300

    [index]: added required intents

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ const Client = require('./structures/Client'),
     Collection = require('./methods/Collection'),
     Server = require('./structures/Server');
 
-global.client = new Client({ partials: ['REACTION', 'MESSAGE'] });
+global.client = new Client({
+    partials: ['REACTION', 'MESSAGE'],
+    intents: ['GUILDS','GUILD_MEMBERS','GUILD_BANS','GUILD_EMOJIS','GUILD_INTEGRATIONS','GUILD_WEBHOOKS','GUILD_INVITES','GUILD_VOICE_STATES','GUILD_PRESENCES','GUILD_MESSAGES','GUILD_MESSAGE_REACTIONS','GUILD_MESSAGE_TYPING','DIRECT_MESSAGES','DIRECT_MESSAGE_REACTIONS','DIRECT_MESSAGE_TYPING']
+});
 global.database = new Database();
 global.server = new Server();
 global.terminal = new Terminal();
