6e72fbdc533147a235e6a7bfc033039871c76208
ApteryxXYZ <apteryx.xyz@gmail.com>
2021-03-08 03:40:48 +1300

    [wolvesville/]: changed file name

---
+++ src/commands/wolvesville/meme.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch');
+
+module.exports = class Meme extends client.commandManager.Command {
+    constructor() {
+        super({
+            name: module.filename.split('/').pop().slice(0, -3),
+            category: module.filename.split('/').slice(-2)[0],
+            aliases: []
+        })
+    }
+
+    async run({ message, args, user }) {
+        fetch('https://werewolf.apteryx.xyz/api/meme')
+            .then(res => res.json())
+            .then(body => {
+                if (body.data) message.send(body.data.url);
+                else message.send(body.error.message)
+            })
+    }
+}
---
+++ src/commands/wolvesville/role.js
@@ -0,0 +1,32 @@
+const Fuse = require('fuse.js');
+
+module.exports = class Role extends client.commandManager.Command {
+    constructor() {
+        super({
+            name: module.filename.split('/').pop().slice(0, -3),
+            category: module.filename.split('/').slice(-2)[0],
+            permissions: { channel: ['EMBED_LINKS', 'USE_EXTERNAL_EMOJIS'] },
+            aliases: [],
+            tags: ['args']
+        })
+    }
+
+    async run({ message, args, user }) {
+        let query = args.join(' ').toLowerCase(),
+            roles = Object.values(message._('roles')),
+            tips = message._('tips'),
+            role = roles.find(r => r.name.toLowerCase() === query) ||
+                roles.find(r => r.abbreviations.toLowerCase().split(', ').includes(query)) ||
+                new Fuse(roles, { keys: ['name', 'abbreviations', 'team', 'aura'] })
+                    .search(query)[0]?.item,
+            tip = tips.find(t => t.role.includes(role.name));
+
+        if (!role) message.send(message._('roleNotFound'));
+        else message.send({ message, title: `${message.emote(role.name.toCamelCase())} ${role.name}`, description: role.description }, [
+            [message._('team'), role.team],
+            [message._('aura'), role.aura],
+            [message._('abbreviations'), role.abbreviations],
+            [message._('tip.tip'), `${tip.content}\n*${tip.credit}*`]
+        ])
+    }
+}
