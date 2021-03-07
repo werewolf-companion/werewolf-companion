4c74ed09bd84346380b936b28f752688c7207e5b
ApteryxXYZ <apteryx.xyz@gmail.com>
2021-03-08 01:16:37 +1300

    [jobs]: fixed message formating

--- src/commands/currency/jobs.js
+++ src/commands/currency/jobs.js
@@ -11,15 +11,7 @@ module.exports = class Jobs extends client.commandManager.Command {
 
     async run({ message, args, user }) {
         let fields = [];
-        for (let job of LIST) {
-            fields.push([
-                job.title.toTitleCase(),
-                `**Payment**: ${job.min} to ${job.max} ${message.emote(job.currency)}
-                **Requirements**:
-                ${job.hours} Hours Worked
-                ${job.daily} Daily Streak`
-            ])
-        }
-        return message.send({ message, title: 'Available Jobs' }, fields);
+        for (let job of LIST) fields.push([job.title.toTitleCase(), `**Payment**: ${job.min} to ${job.max} ${message.emote(job.currency)}\n**Requirements**:\n${job.hours} Hours Worked\n${job.daily} Daily Streak`])
+        return message.send({ message, title: 'Avaiable Jobs', description: `Apply for one of these jobs by using \`${message.prefix}job apply <job title>\`.` }, fields)
     }
 }
