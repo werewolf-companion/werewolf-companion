66f6dbc448ff76a1477e47215a0f5faab20af579
ApteryxXYZ <apteryx.xyz@gmail.com>
2021-03-08 00:29:26 +1300

    [help]: added more information

--- src/commands/core/help.js
+++ src/commands/core/help.js
@@ -8,13 +8,12 @@ module.exports = class Help extends client.commandManager.Command {
     }
 
     async run({ message, args, user }) {
-        return message.send({ message, title: 'Werewolf Companion Help' }, [
-            ['About', `Werewolf Companion is an Android (iOS soon) app and Discord bot made for the players of Werewolf Online, by the players. It contains helpful features such as game information, role information, tips & tricks (submitted by players) media links and more. The Discord bot also comes with a few 'fun' features including an economy system.`],
-            ['Reactions', `Sometimes the bot will response with a reaction rather than a message, here is what those reactions mean;
-            ${message.emote('clock')} : You are being rate limited.
-            ${message.emote('missing')} : Missing required arguments.
-            ${message.emote('forbidden')} : Forbidden, you cannot use this.`],
-            ['Commands', `To view a list of all the bots commands, use \`${message.prefix}commands\`.`]
+        return message.send({ message, title: 'Werewolves Companion Help' }, [
+            ['About', `Wolvesville Companion is an Android (iOS soon) app and Discord bot made for the players of Werewolf Online, by the players. It contains helpful features such as game information, role information, tips & tricks (submitted by players) media links and more. The Discord bot also comes with a few 'fun' features including an economy system.`, false],
+            ['Commands', `I have close to 40 commands you can use, to view a list use \`${message.prefix}commands\`.`, false],
+            ['Reactions', `Sometimes I will response with a reaction rather than a message, here is what those reactions mean:\n${message.emote('clock')} : This command is on cooldown, slow down.\n${message.emote('missing')} : Missing required inputs.\n${message.emote('forbidden')} : Forbidden, you cannot use this.`, false],
+            ['Settings', `I have a few settings you can change, like the prefix. View the settings by using the \`${message.prefix}settings\` command.`, false],
+            ['Custom Prefix', 'If you happen to not know what my prefix in a server is, simply ping me and I\'ll saying my prefix.', false]
         ])
     }
 }
