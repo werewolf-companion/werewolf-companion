618703957d950bb078b10580416559d3c9e929b6
ApteryxXYZ <apteryx.xyz@gmail.com>
2021-03-08 02:50:34 +1300

    [buy]: added arg information

--- src/commands/inventory/buy.js
+++ src/commands/inventory/buy.js
@@ -5,12 +5,12 @@ module.exports = class Buy extends client.commandManager.Command {
         super({
             name: module.filename.split('/').pop().slice(0, -3),
             category: module.filename.split('/').slice(-2)[0],
-            permissions: { channel: ['USE_EXTERNAL_EMOJIS'] },
-            tags: ['args']
+            permissions: { channel: ['USE_EXTERNAL_EMOJIS'] }
         })
     }
 
     async run({ message, args, user }) {
+        if (!args.length) return message.send(`Missing required input \`item id\`.\nTo buy an item use \`${message.prefix}buy <item id> [quantity]\`. View what you can buy using \`${message.prefix}shop\`.`)
         let itemId = args[0].toLowerCase(),
             quantity = Number(args[1]) > 1 && args[1] != 'Infinity' ? Math.ceil(args[1]) : 1,
             balance = user.balance,
@@ -23,9 +23,10 @@ module.exports = class Buy extends client.commandManager.Command {
         if (balance[item.currency] < item.price * quantity) return message.send(`You do not have enough ${item.currency} to buy ${item.name}. You need ${item.price * quantity} ${emote} but only have ${balance[item.currency]} ${emote}.`);
 
         database.users.set(user.id, Object.merge(user, {
-                balance: { [item.currency]: balance[item.currency] - item.price * quantity },
-                inventory: { [item.id]: (inventory[item.id] || 0) + quantity }
-            }))
+            balance: { [item.currency]: balance[item.currency] - item.price * quantity },
+            inventory: { [item.id]: (inventory[item.id] || 0) + quantity }
+        }))
+
         terminal.inventory(`${user.tag} (${user.id}) purchased ${quantity} ${item.name} for ${item.price * quantity} ${item.currency}.`);
         return message.send(`You have successfully purchased ${quantity} ${message.emote(item.path)}.`)
     }
