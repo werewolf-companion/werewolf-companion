29e8e763512166b820f3389a4b7be5c110c729a6
ApteryxXYZ <apteryx.xyz@gmail.com>
2021-03-11 06:35:29 +1300

    [home]: updated home route

--- src/routes/site/home.js
+++ src/routes/site/home.js
@@ -3,8 +3,8 @@ const { render } = require('../fn');
 module.exports = {
     init: app => {
         app.get('/', async (req, res) => {
-            if (req.isAuthenticated()) var self = await client.users.fetch(req.user.id);
-            render(req, res, 'home.ejs', self ? { self } : null);
+            if (req.isAuthenticated()) var self = await client.users.fetch(req.user?.id) || null;
+            render(req, res, 'home.ejs', { self });
         })
 
         app.get('/discord/invite', (req, res) => {
