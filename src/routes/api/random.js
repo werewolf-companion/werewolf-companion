d3fa10c3734a2044a26f8d8bb9d449656b178e34
ApteryxXYZ <apteryx.xyz@gmail.com>
2021-03-11 06:38:48 +1300

    [random]: updated domain

--- src/routes/api/random.js
+++ src/routes/api/random.js
@@ -8,14 +8,15 @@ module.exports = {
             let memeDirectory = resolve('src/public/images/memes'),
                 memes = readdirSync(memeDirectory),
                 meme = memes[Math.floor(Math.random() * memes.length)];
-            return res.status(200).json(code(200, { url: `https://werewolf.apteryx.xyz/images/memes/${meme}` }));
+            return res.status(200).json(code(200, { url: client.constants.links.companion.site +  `/images/memes/${meme}` }));
         })
 
         api.get('/gameplay', (req, res) => {
             let gameplayDirectory = resolve('src/public/images/gameplay'),
                 gameplays = readdirSync(gameplayDirectory),
                 gameplay = gameplays[Math.floor(Math.random() * gameplays.length)];
-            return res.status(200).json(code(200, { url: `https://werewolf.apteryx.xyz/images/gameplay/${gameplay}` }));
+            return res.status(200).json(code(200, { url: client.constants.links.companion.site + `/images/gameplay/${gameplay}` }));
         })
     }
+    
 }
