5d9552437ec90f836a6edb6ab4fd165ac1a59587
ApteryxXYZ <apteryx.xyz@gmail.com>
2021-03-11 08:37:30 +1300

    [guildMember*]: added check for support server

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -4,8 +4,10 @@ module.exports = class GuildMemberAdd extends client.eventManager.Event {
     }
 
     async handle(member) {
-        let user = database.get(member.id, 'user', member.user);
-        database.users.math(user.id, '+', 250, 'balance.gold');
-        terminal.currency(`${user.tag} (${user.id}) joined Wolvesville Companion and earned 250 gold, they now have ${user.balance.gold + 250}`);
+        if (member.guild.id === client.constants.ids.guilds.support) {
+            let user = database.get(member.id, 'user', member.user);
+            database.users.math(user.id, '+', 250, 'balance.gold');
+            terminal.currency(`${user.tag} (${user.id}) joined Wolvesville Companion and earned 250 gold, they now have ${user.balance.gold + 250}.`);
+        }
     }
 }
--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -4,8 +4,10 @@ module.exports = class GuildMemberRemove extends client.eventManager.Event {
     }
 
     async handle(member) {
-        let user = database.get(member.id, 'user', member.user);
-        database.users.math(user.id, '-', 250, 'balance.gold');
-        terminal.currency(`${user.tag} (${user.id}) left Wolvesville Companion and lost 250 gold, they now have ${user.balance.gold - 250}`);
+        if (member.guild.id === client.constants.ids.guilds.support) {
+            let user = database.get(member.id, 'user', member.user);
+            database.users.math(user.id, '-', 250, 'balance.gold');
+            terminal.currency(`${user.tag} (${user.id}) left Wolvesville Companion and lost 250 gold, they now have ${user.balance.gold - 250}.`);
+        }
     }
 }
