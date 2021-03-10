bc31913ae2085a9788bb88ad339837ea9d76a905
ApteryxXYZ <apteryx.xyz@gmail.com>
2021-03-10 18:20:41 +1300

    [links]: changed from constants to hard coded

--- src/commands/core/links.js
+++ src/commands/core/links.js
@@ -9,11 +9,17 @@ module.exports = class Links extends client.commandManager.Command {
     }
 
     async run({ message }) {
-        let links = client.constants.links,
+        let companionLinks = client.constants.links.companion,
+            links = [
+                { title: 'Website', value: `Website for Wolvesville Companion. Providing more information about the bot, app and API.\n[**VISIT**](${companionLinks.site})` },
+                { title: 'Server', value: `Invite link for the Discord support server, join if you need help or just want to chat with other users.\n[**JOIN**](${companionLinks.server})` },
+                { title: 'App', value: `Android app version of the Wolvesville Companion app, even containing features which this bot does not have. iOS version is planned.\n[**DOWNLOAD**](${companionLinks.app})` },
+                { title: 'Bot', value: `Invite link for the Discord bot, which you can iuse to add this bot to your own server.\n[**INVITE**](${companionLinks.bot})` }
+            ],
             fields = [];
 
         for (const link of links)
             fields.push([link.title, link.value]);
-        return message.send({ message, title: 'Wolvesville Companion Links'}, fields);
+        return message.send({ message, title: 'Wolvesville Companion Links' }, fields);
     }
 }
