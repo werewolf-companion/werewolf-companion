a9ca72dd6cdc708f661963eabd8380f58f49a90a
ApteryxXYZ <apteryx.xyz@gmail.com>
2021-03-10 18:21:27 +1300

    [sentence]: added sentence command

---
+++ src/commands/misc/sentence.js
@@ -0,0 +1,15 @@
+const txtgen = require('txtgen');
+
+module.exports = class Eval extends client.commandManager.Command {
+    constructor() {
+        super({
+            name: module.filename.split('/').pop().slice(0, -3),
+            category: module.filename.split('/').slice(-2)[0],
+            cooldown: 6000
+        })
+    }
+
+    async run({ message }) {
+        return message.send(txtgen.sentence());
+    }
+}
