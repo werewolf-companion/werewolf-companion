84e9df02554d0d3316851f4fa9bcdb43db18ca6b
ApteryxXYZ <apteryx.xyz@gmail.com>
2021-03-26 19:06:20 +1300

    [work]: added new "work" type - identify role

--- src/commands/currency/work.js
+++ src/commands/currency/work.js
@@ -1,5 +1,5 @@
 const txtgen = require('random-words'),
-    ms = require('pretty-ms');
+    Fuse = require('fuse.js');
 
 module.exports = class Work extends client.commands.class {
     constructor() {
@@ -16,8 +16,9 @@ module.exports = class Work extends client.commands.class {
         const LIST = client.constants.jobs;
         let job = LIST.find(j => j.title === user.job.title);
         if (!job) return Object.merge(message.send(`You don't have a job, get one using the \`${message.prefix}job apply <job>\` command. You can view a list of jobs using \`${message.prefix}jobs\`.`), { skipCooldown: true });
-        let activities = ['repeatWords'],
+        let activities = ['repeatWords', 'identifyRole'],
             randomActivity = activities[Math.floor(Math.random() * activities.length)];
+
         return this[randomActivity](message, user, job);
     }
 
@@ -28,13 +29,60 @@ module.exports = class Work extends client.commands.class {
             { max, min } = job;
 
         message.send({ message, title: 'Get Working', description: 'Retype the following 5 words:', image: encodeURI(`https://img.shields.io/badge/-${words}-white.png`) });
-        return message.channel.awaitMessages(filter, { max: 1, time, errors: ['time'] }).then(response => {
-            let msg = response.first(),
-                success = msg.content.toLowerCase() === words.toLowerCase();
-            if (!success) max = max / 3, min = min / 3;
-            return this.finishJob(message, user, max, min, job.currency, success);
-        }).catch(response => this.finishJob(message, user, max / 3, min / 3, job.currency, false));
+        return message.channel.awaitMessages(filter, { max: 1, time, errors: ['time'] })
+            .then(response => {
+                let msg = response.first(),
+                    success = msg.content.toLowerCase() === words.toLowerCase();
+                if (!success) max = max / 3, min = min / 3;
+                return this.finishJob(message, user, max, min, job.currency, success);
+            }).catch(response => this.finishJob(message, user, max / 3, min / 3, job.currency, false));
     }
+
+    identifyRole(message, user, job) {
+        const roles = Object.values(message._('roles'));
+
+        let role = roles.random(),
+            roleEmote = client.emote(role.name.toCamelCase()),
+            time = 5000 + (role.name.length * 500),
+            filter = m => m.author.id === user.id,
+            { max, min } = job;
+
+        message.send({ message, title: 'Get Working', description: 'Identify the following role:\n' + roleEmote });
+        return message.channel.awaitMessages(filter, { max: 1, time, errors: ['time'] })
+            .then(response => {
+                let msg = response.first(),
+                    roleNames = [];
+
+                for (const r of roles) {
+                    if (r.name) roleNames.push([r.name, r.name]);
+                    if (r.abbreviations) {
+                        let abbreviations = r.abbreviations.split(',').map(a => a.trim());
+                        for (const abbreviation of abbreviations) roleNames.push([abbreviation, r.name]);
+                    }
+                }
+
+                let foundRole = roleNames.find(r => r[0].toLowerCase() === msg.cleanContent.toLowerCase()),
+                    success = foundRole[1].toLowerCase() === role.name.toLowerCase();
+
+                if (!success) max = max / 3, min = min / 3;
+                return this.finishJob(message, user, max, min, job.currency, success);
+            }).catch(response => this.finishJob(message, user, max / 3, min / 3, job.currency, false));
+    }
+
+    /**
+     * const Fuse = require('fuse.js');
+     * 
+    async run({ message, args, user }) {
+        let query = args.join(' ').toLowerCase(),
+            roles = Object.values(message._('roles')),
+            tips = message._('tips'),
+            role = roles.find(r => r.name.toLowerCase() === query) ||
+                roles.find(r => r.abbreviations.toLowerCase().split(', ').includes(query)) ||
+                new Fuse(roles, { keys: ['name', 'abbreviations', 'team', 'aura'] })
+                    .search(query)[0]?.item,
+            tip = tips.find(t => t.role.includes(role.name));
+
+     */
 
     finishJob(message, user, max, min, currency, success) {
         let payment = Math.floor(Math.random() * (max - min) + min);
