b6439be92b3d541b12fe1fc4a321992ae26a6b67
ApteryxXYZ <apteryx.xyz@gmail.com>
2021-03-26 19:08:03 +1300

    [daily]: add log for topgg reward

--- src/commands/currency/daily.js
+++ src/commands/currency/daily.js
@@ -1,5 +1,5 @@
 const random = require('weighted-random'),
-    ms = require('pretty-ms');
+    ms = require('enhanced-ms');
 
 module.exports = class Daily extends client.commands.class {
     constructor() {
@@ -14,24 +14,27 @@ module.exports = class Daily extends client.commands.class {
 
     async run({ message, user }) {
         let lastDaily = user.cooldowns.daily,
-            twoDays = 86400000 * 2,
+            hours = ms('12h'),
             hasVoted = await client.topgg.hasVoted(user.id),
             streak = user.stats.streak.daily;
 
-        if (Date.now() - lastDaily > twoDays) streak = 1;
+        if (Date.now() - lastDaily > hours) streak = 1;
         else streak++;
+        let streakToSet = streak;
 
         streak = streak + (user.balance.token * 1.5);
         let reward = this.generateReward(streak > 100 ? 100 : streak);
 
         database.users.math(user.id, '+', reward.amount, `balance.${reward.item}`);
-        database.users.set(user.id, streak, 'stats.streak.daily');
-        if (hasVoted) database.users.math(user.id, '+', 1, 'balance.rose');
+        database.users.set(user.id, streakToSet, 'stats.streak.daily');
         terminal.currency(`${user.tag} (${user.id}) claimed their daily reward of ${reward.amount} ${reward.item}, they now have ${user.balance[reward.item] + reward.amount}.`);
 
-        return message.send({
-            message, title: 'Daily Reward', description: `You claimed your daily reward and got ${reward.amount} ${message.emote(reward.item)}. You're on a ${streak} day streak!\n${(hasVoted ? `You also earned an extra ${message.emote('rose')} for voting for the bot!` : `Want to earn extra rewards? Vote for Wolvesville Companion on top.gg by clicking [here](${client.topgg.voteLink})!`)}`
-        })
+        if (hasVoted) {
+            database.users.math(user.id, '+', 1, 'balance.rose');
+            terminal.currency(`${user.tag} (${user.id}) claimed thier weekly reward and voted for the bot, they earned 1 rose, they now have ${user.balance.rose + 1}.`)
+        }
+
+        return message.send({ message, title: 'Daily Reward', description: `You claimed your daily reward and got ${reward.amount} ${message.emote(reward.item)}. You're on a ${streak} day streak!\n${(hasVoted ? `You also earned an extra ${message.emote('rose')} for voting for the bot!` : `Want to earn extra rewards? Vote for Wolvesville Companion on top.gg by clicking [here](${client.topgg.voteLink})!`)}` })
     }
 
     generateReward(streak) {
